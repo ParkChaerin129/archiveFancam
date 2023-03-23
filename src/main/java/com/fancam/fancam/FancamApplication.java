b6b0aa90814e647531cc8752b74bcdd52484c3ae
Park Chaerin <parkchaerin@Parkui-MacBookAir.local>
2023-03-23 16:14:35 +0900

    Initial commit

---
+++ src/main/java/com/fancam/fancam/FancamApplication.java
@@ -0,0 +1,13 @@
+package com.fancam.fancam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FancamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FancamApplication.class, args);
+	}
+
+}
